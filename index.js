ba417738847cf4d3718ec19ad24aaaeec2b6d052
Kelly Selden <kellyselden@gmail.com>
2018-06-25 13:54:41 +0100

    prepend script

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ module.exports = {
     this._super.included.apply(this, arguments);
 
     this.import(require.resolve('three').substr(process.cwd().length + 1), {
-      using: [{ transformation: 'amd', as: 'three' }]
+      using: [{ transformation: 'amd', as: 'three' }],
+      prepend: true
     });
   },
 };
